aff7bc8dc101e2afcfc8fc2532367f773f2ed574
Никита Забелин <NAZabelin@mai.ru>
2024-04-16 01:04:34 +0300

    Night commit

--- src/frontend/frontend/src/components/layouts/Layout.tsx
+++ src/frontend/frontend/src/components/layouts/Layout.tsx
@@ -5,7 +5,7 @@ import {Stack} from "@mui/material";
 
 export default function Layout({children}: { children: React.ReactElement }) {
     return (
-        <Stack sx={{width: "calc(100vw - 40px)", height: "calc(100vh - 40px)", display: "flex", flexDirection: "column", padding: "20px"}} spacing={8}>
+        <Stack sx={{width: "calc(100vw - 100px)", height: "calc(100vh - 40px)", display: "flex", flexDirection: "column", padding: "20px"}} spacing={8}>
             <Header/>
             <main style={{flexGrow: 1}}>{children}</main>
             <Footer/>
--- src/frontend/frontend/src/pages/tasks/[id].tsx
+++ src/frontend/frontend/src/pages/tasks/[id].tsx
@@ -1,7 +1,18 @@
 import {useRouter} from "next/router";
 import React from "react";
-import {Grid, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableRow, Typography} from "@mui/material";
+import {Paper, Stack, Table, TableBody, TableCell, TableContainer, TableRow, Typography} from "@mui/material";
 import {useStore} from "@/store/StoreContext";
+import {MathComponent} from "mathjax-react";
+import Image from "next/image";
+
+import lopatka1 from "@/assets/kirill/1 лопатка, чертеж Кирилла.svg"
+import lopatka2 from "@/assets/kirill/2 лопатка, чертеж Кирилла-1.svg"
+import lopatka3 from "@/assets/kirill/3 лопатка, чертеж Кирилла-1.svg"
+import lopatka4 from "@/assets/kirill/4 лопатка, чертеж Кирилла-1.svg"
+import lopatka5 from "@/assets/kirill/5 лопатка, чертеж Кирилла-1.svg"
+import lopatka6 from "@/assets/kirill/6 лопатка, чертеж Кирилла-1.svg"
+import lopatka7 from "@/assets/kirill/7 лопатка, чертеж Кирилла-1.svg"
+
 
 
 const Task = () => {
@@ -15,13 +26,13 @@ const Task = () => {
     })
 
     if (!task) {
-        return "Not find"
+        return "Not found"
     }
 
     return (<Stack sx={{height: "100%"}}>
 
         <TableContainer component={Paper}>
-            
+
             <Table>
                 <TableBody>
 
@@ -41,32 +52,89 @@ const Task = () => {
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Высота полета</TableCell>
-                        <TableCell>{task.height}</TableCell>
+                        <TableCell>
+
+                            <Stack direction={"row"} spacing={2}>
+                                <Typography>Высота полета</Typography>
+                                <MathComponent tex={"H_{П}"} display={false}/>
+                            </Stack>
+
+                        </TableCell>
+                        <TableCell>
+                            {task.height}
+                        </TableCell>
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Скорость полета</TableCell>
+                        <TableCell>
+                            <Stack direction={"row"} spacing={2}>
+                                <Typography> Скорость полета</Typography>
+                                <MathComponent tex={"M_{П}"} display={false}/>
+                            </Stack>
+
+                        </TableCell>
+
+
                         <TableCell>{task.speed}</TableCell>
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Степень двухконтурности</TableCell>
+                        <TableCell>
+
+                            <Stack direction={"row"} spacing={2}><Typography>Суммарный расход воздуха</Typography>
+                                <MathComponent tex={"G_{_B\\large\\Sigma}"} display={false}/>
+
+                            </Stack>
+
+                        </TableCell>
+                        <TableCell>{task.airConsumption}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <TableCell>
+
+                            <Stack direction={"row"} spacing={2}><Typography>Степень двухконтурности</Typography>
+                                <MathComponent tex={"m"} display={false}/>
+
+                            </Stack>
+
+                        </TableCell>
                         <TableCell>{task.doubleCircuitDegree}</TableCell>
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Степень повышения давления в компрессоре</TableCell>
+                        <TableCell>
+                            <Stack direction={"row"} spacing={2}>
+                                <Typography>Степень повышения давления в компрессоре</Typography>
+
+                                <MathComponent tex={"\\Large\\pi_{k\\large\\Sigma}"}
+                                               display={false}/>
+                            </Stack>
+                        </TableCell>
                         <TableCell>{task.addPressureCompressor}</TableCell>
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Степень повышения давления в вентиляторе</TableCell>
+                        <TableCell>
+
+                            <Stack direction={"row"} spacing={2}><Typography>Степень повышения давления в
+                                вентиляторе</Typography><MathComponent tex={"\\Large\\pi_{\\small B\\large\\Sigma}"}
+                                                                       display={false}/></Stack>
+
+
+                        </TableCell>
                         <TableCell>{task.addPressureFan}</TableCell>
                     </TableRow>
 
                     <TableRow>
-                        <TableCell>Температура газа на входе в турбину</TableCell>
+                        <TableCell>
+
+                            <Stack direction={"row"} spacing={2}><Typography>Температура газа на входе в
+                                турбину</Typography><MathComponent tex={"T^*_Г"}
+                                                                   display={false}/></Stack>
+
+
+                        </TableCell>
                         <TableCell>{task.temperature}</TableCell>
                     </TableRow>
 
@@ -81,11 +149,50 @@ const Task = () => {
                     </TableRow>
 
 
-
                 </TableBody>
             </Table>
-
         </TableContainer>
+        <Stack direction={"row"}>
+            <Image
+                priority
+                src={lopatka1}
+                alt=""
+            />
+            <Image
+                priority
+                src={lopatka2}
+                alt=""
+            />
+            <Image
+                priority
+                src={lopatka3}
+                alt=""
+            />
+
+
+            <Image
+                priority
+                src={lopatka4}
+                alt=""
+            />
+            <Image
+                priority
+                src={lopatka5}
+                alt=""
+            />
+            <Image
+                priority
+                src={lopatka6}
+                alt=""
+            />
+            <Image
+                priority
+                src={lopatka7}
+                alt=""
+            />
+
+        </Stack>
+
     </Stack>)
 }
 
